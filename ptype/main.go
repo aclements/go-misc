1913105245fd76a49715ad8f68b098d30b021d9e
Austin Clements <austin@google.com>
2017-03-07 13:42:09 -0500

    ptype: rename dumptype to ptype

--- ptype/main.go
+++ ptype/main.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Command dumptype prints Go types from a binary using DWARF info.
+// Command ptype prints Go types from a binary using DWARF info.
 package main
 
 import (

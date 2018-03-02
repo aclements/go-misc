456e1c0226ddc78eb83e88795f2d9ff504ab2f3c
Austin Clements <austin@google.com>
2018-03-01 21:40:57 -0500

    split: improve and expand examples

--- split/examples_test.go
+++ split/examples_test.go
@@ -41,15 +41,84 @@ func Example_counter() {
 	counter.Range(func(np *uint64) {
 		sum += atomic.LoadUint64(np)
 	})
+	fmt.Println(sum)
 
 	// Output: 64
-	fmt.Println(sum)
+}
+
+func Example_counterConsistent() {
+	// This example is similar to the "Counter" example, but the
+	// counter goes both up and down. Specifically, each goroutine
+	// increments the counter and then decrements the counter, but
+	// the increment and decrement may happen on different shards.
+	// The sum of the counter at any instant is between 0 and the
+	// number of goroutines, but since Range can't see all of the
+	// shards at the same instant, it may observe a decrement
+	// without an increment, leading to a negative sum.
+	//
+	// In this example, we solve this problem using two-phase
+	// locking.
+	type shard struct {
+		val uint64
+		sync.Mutex
+	}
+	counter := New(func(*shard) {})
+
+	const N = 64
+	var wg sync.WaitGroup
+	var stop uint32
+	for i := 0; i < N; i++ {
+		wg.Add(1)
+		go func() {
+			for atomic.LoadUint32(&stop) == 0 {
+				s := counter.Get().(*shard)
+				s.Lock()
+				s.val++
+				s.Unlock()
+
+				// .. do some work, maybe get moved to
+				// a different shard ..
+				runtime.Gosched()
+
+				s = counter.Get().(*shard)
+				s.Lock()
+				s.val--
+				s.Unlock()
+			}
+			wg.Done()
+		}()
+	}
+
+	// Let the goroutines do some work.
+	time.Sleep(time.Millisecond)
+
+	// Capture a consistent sum by locking all of the shards, then
+	// unlocking all of them. This must be done in a single Range
+	// call to prevent the number of shards from changing.
+	var sum uint64
+	counter.Range(func(s *shard) {
+		s.Lock()
+		sum += s.val
+	}, func(s *shard) {
+		s.Unlock()
+	})
+
+	// Stop the writers.
+	atomic.StoreUint32(&stop, 1)
+	wg.Wait()
+
+	if sum < 0 || sum > N {
+		fmt.Println("bad sum:", sum)
+	}
+	// Output:
 }
 
 func Example_logging() {
 	// This example demonstrates concurrent appends to a split
 	// log. Each shard of the log is protected by a mutex. The log
 	// is combined by sorting the log records in timestamp order.
+	// This example collects a consistent snapshot of the log
+	// using these timestamps.
 	type record struct {
 		when time.Time
 		msg  string
@@ -66,9 +135,12 @@ func Example_logging() {
 		go func(i int) {
 			for j := 0; j < 4; j++ {
 				msg := fmt.Sprintf("goroutine %d message %d", i, j)
-				rec := record{time.Now(), msg}
 				shard := logger.Get().(*shard)
 				shard.Lock()
+				// We have to record the time under
+				// the lock to ensure it's ordered for
+				// the reader.
+				rec := record{time.Now(), msg}
 				shard.log = append(shard.log, rec)
 				shard.Unlock()
 			}
@@ -80,18 +152,30 @@ func Example_logging() {
 	// Collect and sort the log records. This isn't running
 	// concurrently with log appends, but for demonstration
 	// purposes it's written so it could. To get a consistent view
-	// of the log, this uses two-phase locking: it makes two
-	// passes through the shards where the first pass locks all of
-	// the shards and collects the records, and the second pass
-	// unlocks all of the shards.
+	// of the log, this uses timestamp ordering: it records the
+	// current time before calling Range and ignores any records
+	// from after that time. For logs it makes sense to get a
+	// consistent snapshot: a given worker could move between
+	// shards and it would be confusing to see later log records
+	// from that worker without earlier log records.
+	snapshot := time.Now()
 	var combined []record
 	logger.Range(func(val *shard) {
 		val.Lock()
-		combined = append(combined, val.log...)
-	}, func(val *shard) {
+		log := val.log
 		val.Unlock()
+		// Trim records after time "snapshot".
+		i := sort.Search(len(log), func(i int) bool {
+			return log[i].when.After(snapshot)
+		})
+		log = log[:i]
+		combined = append(combined, log...)
 	})
 	sort.Slice(combined, func(i, j int) bool { return combined[i].when.Before(combined[j].when) })
+
+	for _, rec := range combined {
+		fmt.Println(rec.msg)
+	}
 
 	// Unordered output:
 	// goroutine 3 message 0
@@ -110,9 +194,6 @@ func Example_logging() {
 	// goroutine 0 message 1
 	// goroutine 0 message 2
 	// goroutine 0 message 3
-	for _, rec := range combined {
-		fmt.Println(rec.msg)
-	}
 }
 
 func Example_randomSource() {
@@ -146,19 +227,20 @@ func Example_randomSource() {
 	wg.Wait()
 }
 
-func Example_optimisticSnapshot() {
+func Example_optimisticTransactions() {
 	// This example demonstrates computing an instant-in-time
 	// consistent snapshot of a sharded value without blocking
-	// writers. In particular, writers in this example can update
-	// multiple shards transactionally, so this requires careful
-	// synchronization between readers and writers to get a
-	// sequentially consistent view of the entire sharded value.
+	// writers. Writers in this example can update multiple shards
+	// transactionally, so this requires careful synchronization
+	// between readers and writers to get a sequentially
+	// consistent view of the entire sharded value.
 	//
-	// The writer moves "units" between shards: initially all
-	// shards have a count of 0 and the writer repeatedly picks
-	// two shards and transactionally decrements the value of one
-	// shard and increments the value of the other. Hence, at any
-	// instant, the shards should all sum to 0.
+	// Each transaction moves a "unit" between two shards.
+	// Initially all shards have a count of 0. Each writer
+	// repeatedly picks two shards and transactionally decrements
+	// the value of one shard and increments the value of the
+	// other. Hence, at any instant, the shards should all sum to
+	// 0.
 	//
 	// Since the Range callback doesn't see all shards at the same
 	// instant, it can't simply add up the values of the shards.
@@ -169,8 +251,9 @@ func Example_optimisticSnapshot() {
 	// 2. Goroutine 1 calls Range. The callback reads shard 1,
 	// which is 0, and adds 0 to the sum.
 	//
-	// 3. On goroutine 2, a writer atomically moves a unit from
-	// shard 1 to shard 2, so now the shard values are {-1, 1}.
+	// 3. On goroutine 2, a writer transactionally moves a unit
+	// from shard 1 to shard 2, so now the shard values are {-1,
+	// 1}.
 	//
 	// 4. On goroutine 1, the Range continues and the callback
 	// reads shard 2, which has value 1, and adds 1 to the sum.
@@ -186,66 +269,71 @@ func Example_optimisticSnapshot() {
 	// it observed a consistent snapshot.
 
 	type shard struct {
-		val int64  // The unit count of this shard.
-		seq uint64 // Sequence number; the low bit indicates this shard is unstable.
+		order uint32 // Lock order of the shards.
+		val   int64  // The unit count of this shard.
+		seq   uint64 // Sequence number; the low bit indicates this shard is unstable.
 	}
-	val := New(func(*shard) {})
+	var lockOrder uint32
+	val := New(func(s *shard) {
+		s.order = atomic.AddUint32(&lockOrder, 1) - 1
+	})
 
-	// Start a set of goroutines that simply pick shards to move
-	// units between. These don't move the units.
-	shards := make(chan *shard, 64)
+	acquireSeq := func(p *uint64) {
+		// "Acquire" a sequence counter by spinning until the
+		// counter is even and then incrementing it.
+		for {
+			v := atomic.LoadUint64(p)
+			if v&1 == 0 && atomic.CompareAndSwapUint64(p, v, v+1) {
+				return
+			}
+			runtime.Gosched()
+		}
+	}
+
+	// Start a set of writer goroutines.
 	var wg sync.WaitGroup
 	var stop uint32
-	for i := 0; i < cap(shards); i++ {
+	for i := 0; i < 64; i++ {
 		wg.Add(1)
 		go func() {
 			for atomic.LoadUint32(&stop) == 0 {
-				// Pick a shard and send it to the churner.
-				shard := val.Get().(*shard)
-				shards <- shard
-				// We're likely to get the same shard
-				// again if we just loop around.
-				// Perturb the scheduler.
+				// Pick a first shard.
+				shard1 := val.Get().(*shard)
+				// Try to get moved to a different shard.
 				runtime.Gosched()
+				// Pick a second shard.
+				shard2 := val.Get().(*shard)
+				if shard1 == shard2 {
+					continue
+				}
+
+				// Put the shards in lock order.
+				lock1, lock2 := shard1, shard2
+				if lock1.order > lock2.order {
+					lock1, lock2 = lock2, lock1
+				}
+
+				// Lock both shards. Odd sequence
+				// numbers also indicates their values
+				// are unstable.
+				acquireSeq(&lock1.seq)
+				acquireSeq(&lock2.seq)
+
+				// Move a unit from shard1 to shard2.
+				atomic.AddInt64(&shard1.val, -1)
+				atomic.AddInt64(&shard2.val, +1)
+
+				// Increment the sequence numbers
+				// again to indicate the shards
+				// changed, but are now stable.
+				atomic.AddUint64(&lock1.seq, 1)
+				atomic.AddUint64(&lock2.seq, 1)
 			}
 			wg.Done()
 		}()
 	}
 
-	// Start a goroutine that moves units between the shards
-	// picked by the above goroutines.
-	go func() {
-		for {
-			// Pick a pair of shards.
-			shard1, ok := <-shards
-			if !ok {
-				break
-			}
-			shard2, ok := <-shards
-			if !ok {
-				break
-			}
-			if shard1 == shard2 {
-				continue
-			}
-
-			// Increment the sequence number on both
-			// shards to indicate their values are
-			// unstable.
-			atomic.AddUint64(&shard1.seq, 1)
-			atomic.AddUint64(&shard2.seq, 1)
-
-			// Move a unit from shard1 to shard2.
-			atomic.AddInt64(&shard1.val, -1)
-			atomic.AddInt64(&shard2.val, +1)
-
-			// Increment the sequence number again to
-			// indicate the shards changed, but are now
-			// stable.
-			atomic.AddUint64(&shard1.seq, 1)
-			atomic.AddUint64(&shard2.seq, 1)
-		}
-	}()
+	// Let the writers get going.
 	time.Sleep(time.Millisecond)
 
 	// Retrieve a consistent sum of the shards. The sum should be
@@ -308,10 +396,9 @@ func Example_optimisticSnapshot() {
 	// Exit all workers.
 	atomic.StoreUint32(&stop, 1)
 	wg.Wait()
-	close(shards)
 
-	// Output: Values sum to 0
 	fmt.Printf("Values sum to %d\n", valSum)
+	// Output: Values sum to 0
 }
 
 // TODO: SRCU-style grace period algorithm? Consistent counter using
